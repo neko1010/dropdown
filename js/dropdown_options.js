a74e44ccd71284a8d450e89c6a285c86a4e9cbad
Nick Kolarik <nicholaskolarik@gmail.com>
2018-06-21 10:21:09 -0400

    first commit

---
+++ js/dropdown_options.js
@@ -0,0 +1,7 @@
+var dropdown_options = [
+	{ value: "composite",
+		text: "Total Overhead (%)" },
+	{ value: "cost_center",
+		text: "Cost Center Overhead (%)"},
+	{ value: "facility",
+		text: "Facility Overhead (%)"}]

4ead8c11d1133b7fa12526346a2394c1b75f5cfc
neko1010 <nicholaskolarik@gmail.com>
2018-06-21 13:03:40 -0400

    Add files via upload

---
+++ map-funding.js
@@ -0,0 +1,201 @@
+var dropdown_options = [
+	{ value: "composite",
+		text: "Total Overhead (%)" },
+	{ value: "cost_center",
+		text: "Cost Center Overhead (%)"},
+	{ value: "facility",
+		text: "Facility Overhead (%)"}]
+// populate drop-down
+d3.select("#dropdown")
+	.selectAll("option")
+	.data(dropdown_options)
+	.enter()
+	.append("option")
+	.attr("value", function(option) { return option.value; })
+	.text(function(option) { return option.text; });
+// initial dataset on load
+var selected_dataset = "composite";
+// set width and height of svg element
+var width = 1140;
+var height = 500;
+
+// create projection
+var projection = d3.geo.albersUsa()
+	.translate([width / 2, height / 2])
+	.scale([1100]);
+
+// create path generator; converts geojson to svg path's ("M 100 100 L 300 100 L 200 300 z")
+var path = d3.geo.path()
+	.projection(projection);
+
+// create an svg element to the body of the html
+var svg = d3.select("#map").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+// add a tooltip
+var tooltip = d3.select("body")
+	.append("div")
+	.attr("class", "tooltip");
+
+// create a quantize scale (function) to sort data values into buckets of color
+var color = d3.scale.quantize()
+	.range(colorbrewer.Greens[5])
+
+// make a legend
+var legend = d3.select("#legend")
+	.append("ul")
+	.attr("class", "list-inline");
+
+var dropDown = d3.select("#dropdown");
+
+dropDown.on("change", function() {
+
+	// newly selected dataset includes downtown
+	//d3.select("#downtown")
+
+	selected_dataset = d3.event.target.value;
+
+	plot.call(updateFill, selected_dataset)
+
+});
+/* function updateFill(selection, selected_dataset) {
+
+            var d_extent = d3.extent(selection.data(), function(d) {
+                return parseFloat(d.properties[selected_dataset]);
+            });
+
+            rescaleFill(selection, d_extent);
+        }
+ */
+// function to calculate a color based on the data/2018-usgs-water-science-centers-total-funding.csv file
+function calculate_color(d) {
+
+	var value = d.selected_dataset;
+
+	if (value) {
+		return color(value);
+	} else {
+		return "#ccc"; // grayish
+	}
+}
+
+// load the data
+d3.csv("data/2018-usgs-water-science-centers-total-funding.csv", function(funding_data) {
+
+	// set the input domain for the color scale
+	color.domain([
+		d3.min(funding_data, function(d) {	return parseFloat(d.selected_dataset); }),
+		//0 , // 56% is lowest (WI)
+		d3.max(funding_data, function(d) { return (d.selected_dataset); })
+		]);
+
+	// load the data file; note path is relative from index.html
+	d3.json("data/us-states.json", function(error, json) {
+
+		if (error) { return console.error(error) };	
+
+		// merge the data and geojson
+		for (var i = 0; i < funding_data.length; i++) {
+
+			// get the state name
+			var funding_data_state = funding_data[i].state;
+
+			// get the data values and convert from string to float
+			var wsc_value = (funding_data[i].wsc);//leave as string
+			var composite_data_value = parseFloat(funding_data[i].composite);
+			var cost_center_data_value = parseFloat(funding_data[i].cost_center);
+			var facility_data_value = parseFloat(funding_data[i].facility);
+
+			// find the corresponding state inside the geojson
+			for (var j = 0; j < json.features.length; j++) {
+
+				// get the json state name
+				var json_data_state = json.features[j].properties.name;
+
+				if (funding_data_state === json_data_state) {
+
+					// copy the data value into the the json
+					json.features[j].properties.wsc = wsc_value;
+					json.features[j].properties.composite = composite_data_value;
+					json.features[j].properties.cost_center = cost_center_data_value;
+					json.features[j].properties.facility = facility_data_value;					
+
+					// stop looking through the geojson
+					break;
+				}
+			}	
+		}
+		
+		// bind the data and create one path for each geojson feature
+		svg.selectAll("path")
+			.data(json.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.attr("fill", calculate_color);
+		plot = svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("stroke", "#808080")
+            .attr("fill", "#b3b3b3")
+            .call(calculate_color, selected_dataset)
+		svg.selectAll("path")
+			.data(json.features)
+			.on("mouseover", function(d) {
+				d3.select(this)
+					.transition().duration(500)
+					.attr("fill", "orange")
+					.attr("stroke-width", 3)
+				d3.select("#state_name").text(d.properties.name)
+				d3.select("#wsc").text("WSC: " + (d.properties.wsc))
+				d3.select("#State Composite").text("Composite: " + d3.format(".2f,%")(d.properties.composite))
+				d3.select("#State Cost Center").text("CostCenter: " + d3.format(".2f,%")(d.properties.cost_center))
+				d3.select("#state_facility").text("Facility: " + d3.format(".2f,%")(d.properties.facility));
+			})
+			.on("mouseout", function(d) {
+				d3.select(this)
+					.transition().duration(500)
+					.attr("fill", calculate_color)
+					.attr("stroke-width", 1)
+		  		return tooltip.style("visibility", "hidden");
+			})
+			.on("click", function(d) {	// display a tooltip
+		  		return tooltip.style("visibility", "visible")
+		  				.style("top", (d3.event.pageY + 10) + "px")
+		  				.style("left", (d3.event.pageX + 10) + "px")
+		  				.html("<h2>" + d.properties.name + "</h2>" + 
+		  					  "<br\>" +
+		  					  "<h3>WSC: " + (d.properties.wsc) + "</h3>" + 
+		  					  "<hr>" +
+		  					  "<h4>Composite: " + d3.format(".2f,%")(d.properties.composite) + "</h4>" + 
+		  					  "<h4>Cost Center: " + d3.format(".2f,%")(d.properties.cost_center) + "</h4>" +
+		  					  "<h4>Facility: " + d3.format(".2f,%")(d.properties.facility) + "</h4>");
+
+		  	})
+		  	.on("mousemove", function() {
+		  		return tooltip.style("top", (event.pageY + 10) + "px").style("left", (event.pageX + 10) + "px");
+		  	})
+		function updateFill(selection, selected_dataset) {
+
+            var d_extent = d3.extent(selection.data(), function(d) {
+                return parseFloat(d.properties[selected_dataset]);
+            });
+
+            rescaleFill(selection, d_extent);
+        }
+		var keys = legend.selectAll("li.key")
+			.data(color.range())
+
+		keys.enter().append("li")
+			.attr("class", "key")
+			.style("border-top-color", String)
+			.text(function(d) {
+				var r = color.invertExtent(d);
+				var format = d3.format(".2f,%");
+				return format(+r[0]) + " - " + format(+r[1]);
+			});
+	});
+});
